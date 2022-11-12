6e243dfb91a892805df5ac1f730b6e9097d9e7b2
vagashkov <vitali.agashkov@gmail.com>
2022-11-12 18:01:05 +0300

    Add PostgreSQL database support

--- src/main.py
+++ src/main.py
@@ -1,23 +1,57 @@
-from starlite import Starlite, Controller, get
+import asyncio
 
-employees_data = {"staff_list": [
-    {"id": 1, 'post': "CEO", 'name': "Ivan Marakasoff"},
-    {'id': 2, 'post': "CLO", 'name': "Top Lawyer"},
-    {'id': 3, 'post': "CIO", 'name': "Kilobit Megabitov"},
-    {'id': 4, 'post': "CFO", 'name': "Giveme Moremoney"}]
-}
+# from asyncpg import connect, Connection
+from sqlalchemy import Column, Float, Integer, String, select
+from sqlalchemy.orm import Mapped, Session, declarative_base
+from starlite import Controller, DTOFactory, HTTPException, Starlite, get
+from starlite.plugins.sql_alchemy import SQLAlchemyConfig, SQLAlchemyPlugin
+from starlite.status_codes import HTTP_404_NOT_FOUND
+from typing import cast, Optional
+
+
+Base = declarative_base()
+
+sqlalchemy_config = SQLAlchemyConfig(connection_string="postgresql://staff:staff_pwd@localhost/staff", use_async_engine=False)
+sqlalchemy_plugin = SQLAlchemyPlugin(config=sqlalchemy_config)
+dto_factory = DTOFactory(plugins=[sqlalchemy_plugin])
+
+
+class Employee(Base):
+    __tablename__ = "employees"
+    id: Mapped[int] = Column(Integer, primary_key=True)
+    name: Mapped[str] = Column(String)
+    post: Mapped[float] = Column(String)
+
+
+def on_startup() -> None:
+    """Initialize the database."""
+    Base.metadata.create_all(sqlalchemy_config.engine)
 
 
 class EmployeeController(Controller):
-    path = '/employees'
+    '''
+    Manages employees info (retrieve from database etc).
+    '''
 
-    @get()
-    def employees_list(self) -> dict[str, str]:
-        return employees_data
+    @get(path='/employees/')
+    def employees_list(self, db_session: Session) -> dict[str, str]:
+        """Get a company by its ID and return it.
+            If a company with that ID does not exist, return a 404 response
+            """
+        employees: Optional[Employee] = db_session.scalars(select(Employee)).all()
+        if not employees:
+            raise HTTPException(detail=f"No employees found", status_code=HTTP_404_NOT_FOUND)
+        return employees
 
-    @get("/{emp_id: int}")
-    def employee_details(self, emp_id: int) -> dict[str, str]:
-        return employees_data['staff_list'][emp_id]
+    @get(path="/employees/{employee_id:int}/")
+    def get_employee(self, employee_id: str, db_session: Session) -> Employee:
+        """Get an employee by its ID and return it.
+        If it doesn't with that ID does not exist, return a 404 response
+        """
+        employee: Optional[Employee] = db_session.scalars(select(Employee).where(Employee.id == employee_id)).one_or_none()
+        if not employee:
+            raise HTTPException(detail=f"Company with ID {employee_id} not found", status_code=HTTP_404_NOT_FOUND)
+        return employee
 
 
-app = Starlite(route_handlers=[EmployeeController])
+app = Starlite(route_handlers=[EmployeeController], on_startup=[on_startup], plugins=[sqlalchemy_plugin])
--- src/tests/test_staff.py
+++ src/tests/test_staff.py
@@ -3,18 +3,20 @@ from starlite.testing import TestClient
 
 from ..main import app
 
+test_data = {"id": 1, 'post': "CEO", 'name': "Ivan Marakasoff"}
+
 
 def test_employees_list(test_client: TestClient):
     with TestClient(app=app) as client:
-        response = client.get("/employees")
+        response = client.get("/employees/")
         assert response.status_code == HTTP_200_OK
-        assert len(response.json()['staff_list']) == 4
-        assert response.json()['staff_list'][-1]['name'] == 'Giveme Moremoney'
+        assert len(response.json()) == 4
+        assert response.json()[-1]['name'] == 'Giveme Moremoney'
 
 
-def test_employee_details(test_client: TestClient):
-    test_data = {"id": 1, 'post': "CEO", 'name': "Ivan Marakasoff"}
-    with TestClient(app=app) as client:
-        response = client.get("/employees/0")
-        assert response.status_code == HTTP_200_OK
-        assert response.json() == test_data
+# def test_employee_details(test_client: TestClient):
+#     test_data = {"id": 1, 'post': "CEO", 'name': "Ivan Marakasoff"}
+#     with TestClient(app=app) as client:
+#         response = client.get("/employees/0")
+#         assert response.status_code == HTTP_200_OK
+#         assert response.json() == test_data
