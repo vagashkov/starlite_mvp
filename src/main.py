cbd5d853a3fc9e7cf9f7c1c2a47b05053b82ef37
vagashkov <vitali.agashkov@gmail.com>
2022-11-23 19:18:40 +0300

    Add employee creation

--- src/main.py
+++ src/main.py
@@ -1,26 +1,24 @@
-import asyncio
-import asyncpg
-
-# from asyncpg import connect,
+from datetime import datetime
 from os import environ
-from sqlalchemy import Column, Integer, String, select
-from sqlalchemy.orm import Mapped, declarative_base
-from sqlalchemy.pool import NullPool
-from sqlalchemy.ext.asyncio import create_async_engine
+from typing import Any
 
-from starlite import State, Controller, HTTPException, Starlite
-from starlite.handlers import get, post, patch, delete
+
+from sqlalchemy import MetaData, Table, Column, Integer, String, DateTime, select, insert, delete
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.orm import Mapped, declarative_base
+
+
+from starlite import Starlite, State, DTOFactory, Controller, Response, HTTPException
+from starlite.handlers import get, post
 from starlite.plugins.sql_alchemy import SQLAlchemyConfig, SQLAlchemyPlugin
 from starlite.status_codes import HTTP_404_NOT_FOUND
-from starlite.types import Partial
 
 
-Base = declarative_base()
+# Define database connection credentials
 db_host = environ.get("POSTGRES_HOST", "localhost")
 db_user = environ.get("POSTGRES_USER", "postgres")
 db_password = environ.get("POSTGRES_PASSWORD", "postgres_password")
 db_name = environ.get("POSTGRES_DB", "staff")
-
 sqlalchemy_config = SQLAlchemyConfig(
     connection_string="postgresql://{}:{}@{}/{}".format(
         db_user, db_password, db_host, db_name
@@ -28,14 +26,39 @@ sqlalchemy_config = SQLAlchemyConfig(
     use_async_engine=False,
 )
 sqlalchemy_plugin = SQLAlchemyPlugin(config=sqlalchemy_config)
-# dto_factory = DTOFactory(plugins=[sqlalchemy_plugin])
+dto_factory = DTOFactory(plugins=[sqlalchemy_plugin])
+
+# # Use Core method to describe db structure
+# metadata = MetaData()
+# employees_table = Table('employees', metadata,
+#                         Column('id', Integer(), primary_key=True, autoincrement=True),
+#                         Column('name', String(50), nullable=False, index=True),
+#                         Column('post', String(10), nullable=False),
+#                         Column('created_on', DateTime(), default=datetime.now),
+#                         Column('updated_on', DateTime(), default=datetime.now, onupdate=datetime.now))
+
+
+Base = declarative_base()
 
 
 class Employee(Base):
     __tablename__ = "employees"
     id: Mapped[int] = Column(Integer, primary_key=True)
     name: Mapped[str] = Column(String)
-    post: Mapped[float] = Column(String)
+    post: Mapped[str] = Column(String)
+
+
+CreateEmployeeDTO = dto_factory("CreateEmployeeDTO", Employee)
+
+
+class EmployeeResponse(Response):
+    def serializer(self, value: Any):
+        if isinstance(value[0], Employee):
+            return [
+                {"id": record.id, "name": record.name, "post": record.post}
+                for record in value
+            ]
+        return super().serializer(value)
 
 
 async def on_startup(state: State) -> None:
@@ -44,12 +67,12 @@ async def on_startup(state: State) -> None:
         "postgresql+asyncpg://{}:{}@{}/{}".format(
             db_user, db_password, db_host, db_name
         ),
-        poolclass=NullPool,
         echo=True,
     )
     # async with engine.begin() as conn:
     # await conn.run_sync(Base.metadata.drop_all)
     # await conn.run_sync(Base.metadata.create_all)
+    # metadata.create_all(engine)
     state.engine = engine
 
 
@@ -65,8 +88,15 @@ class EmployeeController(Controller):
     """
     path = "/employees"
 
-    @get()
-    async def employees_list(self, state: State) -> dict:
+    @get(path='/delete/')
+    async def delete_employees(self, state: State) -> None:
+        """Get employees list and return it."""
+        async with state.engine.connect() as conn:
+            await conn.execute(delete(Employee))
+            await conn.commit()
+
+    @get(name="employees_list", response_class=EmployeeResponse)
+    async def employees_list(self, state: State) -> list[Employee]:
         """Get employees list and return it."""
         async with state.engine.connect() as conn:
             result = await conn.execute(select(Employee))
@@ -75,14 +105,15 @@ class EmployeeController(Controller):
                     detail=f"No employees found", status_code=HTTP_404_NOT_FOUND
                 )
             records_list = result.fetchall()
+            # biuld JSON based on list of sqlalchemy.engine.row.Row objects
             employees = [
-                {"id": record.id, "name": record.name, "post": record.post}
-                for record in records_list
+                 {"id": record.id, "name": record.name, "post": record.post}
+                 for record in records_list
             ]
             return employees
 
-    @get(path="/{employee_id:str}/")
-    async def get_employee(self, employee_id: int, state: State) -> dict:
+    @get(path="/{employee_id:int}/", name="employee_details", response_class=EmployeeResponse)
+    async def get_employee(self, employee_id: int, state: State) -> Employee:
         """Get an employee by its ID and return it.
         If it doesn't with that ID does not exist, return a 404 response
         """
@@ -100,16 +131,22 @@ class EmployeeController(Controller):
             return employees
 
     @post()
-    async def create_employee(self, data: Employee) -> Employee:
-        pass
+    async def create_employee(self, data: CreateEmployeeDTO, state: State) -> Any:
+        async with state.engine.connect() as conn:
+            employee: Employee = data.to_model_instance()
+            result = await conn.execute(insert(Employee).values(id=employee.id, name=employee.name, post=employee.post))
+            await conn.commit()
+            return employee
+
+
 
     # @patch(path="/{employee_id:str}/")
     # async def update_employee(self, data: Partial[Employee]) -> None:
     #     pass
 
-    @delete(path="/{employee_id:str}/")
-    async def delete_employee(self, data: Employee) -> None:
-        pass
+    # @delete(path="/{employee_id:str}/")
+    # async def delete_employee(self, data: Employee) -> None:
+    #     pass
 
 
 app = Starlite(
--- src/tests/test_staff.py
+++ src/tests/test_staff.py
@@ -1,4 +1,5 @@
-from starlite.status_codes import HTTP_200_OK
+import pytest
+from starlite.status_codes import HTTP_200_OK, HTTP_201_CREATED
 from starlite.testing import TestClient
 
 from ..main import app
@@ -6,23 +7,41 @@ from ..main import app
 
 test_client = TestClient(app=app)
 
+test_data = [
+    {"id": 1, "name": "Chief Commander", "post": "CEO"},
+    {"id": 2, "name": "Legal Warrior", "post": "CLO"},
+    {"id": 3, "name": "Money Maker", "post": "CFO"},
+    {"id": 4, "name": "Nuts von Bolts", "post": "CAO"},
+    {"id": 5, "name": "Luke Skywalker", "post": "CTO"}]
+
 
 def test_employees_list():
     with test_client:
+        clear_employees(test_client)
+        check_create_employee(test_client)
         check_employees(test_client)
         check_employee_details(test_client)
+
+
+def clear_employees(client: TestClient):
+    response = client.get("/employees/delete/")
 
 
 def check_employees(client: TestClient):
     response = client.get("/employees/")
     assert response.status_code == HTTP_200_OK
-    assert len(response.json()) == 5
-    assert response.json()[-1]["name"] == "Chair fon Table"
+    assert len(response.json()) == len(test_data)
+    assert response.json()[-1] == test_data[-1]
 
 
 def check_employee_details(client: TestClient):
-    test_data = [{"id": 1, 'post': "CEO", 'name': "Ivan Marakasoff"}]
     response = client.get("/employees/1/")
     assert response.status_code == HTTP_200_OK
-    assert response.json() == test_data
+    assert response.json()[0] == test_data[0]
 
+
+def check_create_employee(client: TestClient):
+    for record in test_data:
+        response = client.post("/employees/", json=record)
+        assert response.status_code == HTTP_201_CREATED
+        assert response.json() == record
